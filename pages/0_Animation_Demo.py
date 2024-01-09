ccdd6c29a87a353ba3a07d14640f0bcd47e6eeb6
avlozza <68394457+avlozza@users.noreply.github.com>
2024-01-09 13:33:01 +0000

    V1.2

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,84 +1,95 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+from datetime import datetime
+import jyotishyamitra as jsm
+from geopy.geocoders import Nominatim
+from geopy.exc import GeocoderTimedOut
+from timezonefinder import TimezoneFinder
 
+def display_chart_details(chart_details):
+    col1, col2 = st.columns(2)
+    with col1:
+        # Displaying Ascendant details
+        ascendant_details = chart_details['ascendant']
+        # Format the position in DDD° MM' SS.S" format
+        pos_deg = ascendant_details['pos']['deg']
+        pos_min = ascendant_details['pos']['min']
+        pos_sec = ascendant_details['pos']['sec']
+        formatted_pos = f"{pos_deg}° {pos_min}' {pos_sec:.1f}\""
+        # Create a new dictionary with the formatted position
+        ascendant_details['pos'] = {'formatted': formatted_pos}
+        df_ascendant = pd.DataFrame([ascendant_details]).T
+        st.write("Ascendant Details:")
+        st.write(df_ascendant)
+    with col2:
+        # Displaying Classifications details
+        classifications_details = chart_details['classifications']
+        df_classifications = pd.DataFrame([classifications_details]).T
+        st.write("Classifications Details:")
+        st.write(df_classifications)
 
-def animation_demo() -> None:
+    # Displaying Planets details
+    planets_details = chart_details['planets']
+    df_planets = pd.DataFrame(planets_details).T
 
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
+    # Formatting aspects data in a readable format
+    df_planets['Aspects_Houses'] = df_planets['Aspects'].apply(lambda x: x.get('houses', []))
+    df_planets['Aspects_Planets'] = df_planets['Aspects'].apply(lambda x: x.get('planets', []))
+    df_planets['Aspects_Signs'] = df_planets['Aspects'].apply(lambda x: x.get('signs', []))
 
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
+    # Dropping the original 'Aspects' column
+    df_planets.drop(['Aspects', 'status'], axis=1, inplace=True)
 
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
+    # Displaying the modified Planets details DataFrame
+    st.write("Planets Details:")
+    st.write(df_planets)
 
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
+    # Displaying Houses details
+    houses_details = chart_details['houses']
+    df_houses = pd.DataFrame(houses_details)
+    st.write("Houses Details:")
+    st.write(df_houses)
 
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
+with st.sidebar.expander("Birth Details"):
+    # User input for Name
+    name = st.text_input("Name","Garima")
 
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
+    # User input for Gender
+    gender = st.radio("Gender", ["Male", "Female"], horizontal=True)
 
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
+    # User input for Date of Birth
+    dob = st.date_input("Birth Date", value=datetime(1992,4,27),min_value=datetime(1990, 1, 1))
+    tob = st.time_input("Birth Time")
 
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
+    # User input for Place of Birth
+    pob = st.text_input("Place of Birth","New Delhi,India")
 
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
+    # Get latitude, longitude, and timezone details using geopy
+    lat = st.number_input("Enter Latitude:", -90.0, 90.0, +28.6444, 0.0001)
+    long = st.number_input("Enter Longitude:", -180.0, 180.0, +77.216, 0.0001)
+    tz = st.number_input("Enter Timezone:", -12.0, 12.0, 5.5, 0.5)
 
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+    # Providing input birth data
+    inputdata = jsm.input_birthdata(name=name, gender=gender,year=dob.year, month=dob.month, day=dob.day,place=pob, longitude=long, lattitude=lat, timezone=tz,hour=tob.hour, min=tob.minute, sec=tob.second)
 
+    # Validate Birthdata
+    jsm.validate_birthdata()
 
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
+# If Birthdata is valid, get birthdata
+if jsm.IsBirthdataValid():
+    birthdata = jsm.get_birthdata()
 
-animation_demo()
+    # Invoke the API generate_astrologicalData with returnval desired to be dictionary
+    astrodata = jsm.generate_astrologicalData(birthdata, returnval="ASTRODATA_DICTIONARY")
 
-show_code(animation_demo)
+    # Display a dropdown to select a specific chart
+    if astrodata:
+        selected_chart = st.sidebar.selectbox("Select Chart:", list(astrodata.keys()))
+
+        # Display details for the selected chart
+        chart_details = astrodata[selected_chart]
+        st.sidebar.write(chart_details)
+
+        # Extracting specific information for display in 6 dataframes
+        if selected_chart in ["D1", "D2", "D3", "D4", "D7","D9", "D10","D12","D16","D20","D24","D27","D30","D40","D45","D60","Balas","AshtakaVarga","Dashas","special_points","user_details"]:  # Add other charts as needed
+            display_chart_details(chart_details)
